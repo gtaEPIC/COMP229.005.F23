115fdd920b3a9f424f3eba65f7822bceb2c5eb9a
JViniX <47263194+JViniX@users.noreply.github.com>
2023-09-27 19:55:41 -0400

    Week 03 - Review

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -15,3 +15,23 @@ module.exports.about = function (req, res, next) {
         }
     );
 }
+
+module.exports.project = function (req, res, next) {
+    res.render('projserv',
+        {
+            title: 'Projects Page',
+            projName: 'Website',
+            desc: 'This is a example project'
+        }
+    );
+}
+
+module.exports.service = function (req, res, next) {
+    res.render('projserv',
+        {
+            title: 'Services Page',
+            projName: 'Web development',
+            desc: 'I am able to develop your web application.'
+        }
+    );
+}
